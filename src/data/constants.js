ec2c77e6e6c13067bd23c25bc77200b0d6a25e7d
Sohail <sohaildavoodi41@gmail.com>
2024-12-12 15:21:01 +0430

    finish the navbar ,and Add social links Myself

---
+++ src/data/constants.js
@@ -0,0 +1,18 @@
+export const Bio = {
+  name: "Sohail",
+  roles: [
+    "Full Stack Developer",
+    "Android Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/SohailDawoodi",
+  resume:
+    "https://drive.google.com/file/d/1fbbcxF13d76V7qFzi-u0ZbYDHd9eqlff/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+  telegram: "https://t.me/RishavChanda",
+  insta: "https://www.instagram.com/rishav_chanda/",
+  facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
