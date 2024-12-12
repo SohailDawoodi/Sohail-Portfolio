8151444f5bb8b4e6ecb16e00634fbaa7d341da0b
Sohail <sohaildavoodi41@gmail.com>
2024-12-12 15:28:47 +0430

    Finish the navbar and add my social media links

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,17 +2,19 @@ export const Bio = {
   name: "Sohail",
   roles: [
     "Full Stack Developer",
-    "Android Developer",
+    "SoftWare Engineer",
     "UI/UX Designer",
     "Programmer",
+    "Project Manager",
+    "Monitor",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/SohailDawoodi",
   resume:
     "https://drive.google.com/file/d/1fbbcxF13d76V7qFzi-u0ZbYDHd9eqlff/view?usp=drive_link",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  telegram: "https://t.me/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+  linkedin: "https://www.linkedin.com/in/sohail-dawoodi-6286a2307?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  telegram: "https://t.me/Sohail_afg_786",
+  instagram: "https://www.instagram.com/sohail_dawoodi_786/profilecard/?igsh=MTQ1ZGwyZTZpNHpjYg==",
+  facebook: "https://www.facebook.com/msohail.dawoodi?mibextid=ZbWKwL",
 };
