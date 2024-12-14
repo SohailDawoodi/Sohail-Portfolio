64f5538c7643b9b1957811819570774cb0e347c1
Sohail <sohaildavoodi41@gmail.com>
2024-12-15 02:34:01 +0430

    Add and develop the project component,also give style.

---
+++ src/components/Projects/index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+        This section showcases my work across various fields, including Web Development, UI/UX Design, Android App Development, and Project Management. Each project reflects my skills and experience in delivering user-friendly, creative, and efficient solutions.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'web app' ?
+            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+            :
+            <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'android app' ?
+            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
+            :
+            <ToggleButton value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'project manager' ?
+            <ToggleButton active value="project manager" onClick={() => setToggle('project manager')}>PROJECT MANAGER</ToggleButton>
+            :
+            <ToggleButton value="project manager" onClick={() => setToggle('project manager')}>PROJECT MANAGER</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'ui/ux design' ?
+            <ToggleButton active value="ui/ux design" onClick={() => setToggle('ui/ux design')}>UI/UX DESIGN</ToggleButton>
+            :
+            <ToggleButton value="ui/ux design" onClick={() => setToggle('ui/ux design')}>UI/UX DESIGN</ToggleButton>
+          }
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category === toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
