e6368b29a2ca8859a035d3a58c28248b381447df
Sohail <sohaildavoodi41@gmail.com>
2024-12-15 03:04:22 +0430

    Develop the App.js and add projectand project detail to this file.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience"
 import Education from "./components/Education";
+import Projects from "./components/Projects";
+import ProjectDetails from "./components/ProjectDetails";
+
+
 
 import styled from "styled-components";
 
@@ -38,7 +42,12 @@ function App() {
           <Experience />
         </Wrapper>
         <Education />
-       
+       <Wrapper>
+       <Projects openModal={openModal} setOpenModal={setOpenModal} />
+       </Wrapper>
+       {openModal.state &&
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          }
       </Body>
     </Router>
   </ThemeProvider>
